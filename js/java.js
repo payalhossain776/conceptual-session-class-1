838c5e4aad60fb9875075a449f8e4e8e4a16f6ed
payal hossain <payalhossain508@gmail.com>
2024-11-02 06:18:39 +0600

    first line

---
+++ js/java.js
@@ -0,0 +1,101 @@
+// titel-1 getElementById.......
+// const titail =document.getElementById('title');
+// titail.innerText='plz add my show pis';
+// titail.style.backgroundColor ='goldenrod'
+// console.log(titail);
+
+//scrollby........
+// function kalapotiscrollbtn(){
+//       window.scrollBy(0,50)
+// }
+
+//tital-2 querySelector........
+// const tatail2=document.querySelector('#title2');
+// tatail2.textContent='hello what are you doing'
+// tatail2.style.backgroundColor='red'
+// console.log(tatail2);
+
+// // item getElementsByClassName.............
+// const item =document.getElementsByClassName('li-item')
+// for(const items of item){
+//       items.style.color='green';
+// }
+
+// item-arrey to html collectiom Array.......
+// const arry =Array.from(item);
+// console.log(arry); 
+
+// body tag what element........
+// const bodyName =document.querySelector('body');
+// console.log(bodyName.children);
+
+//parentnode.....
+// const ulName= document.getElementById('li-container');
+// console.log(ulName.parentElement);
+
+//now mnuplting to html to java......
+
+//li innerText.......
+// const li1 =document.querySelector('ul');
+// console.log(li1.innerText);
+
+// element.inner.Html.......
+// const ul2 =document.querySelector('ul');
+// const li =document.createElement('li');
+// li.innerText='amr ki mn nai';
+// ul2.appendChild(li)
+
+// textContent.......
+// const li1 =document.querySelector('ul');
+// console.log(li1.textContent);
+
+// classList.......
+// const ul2 =document.querySelector('ul');
+// const li =document.createElement('li');
+// li.innerText='amr ki mn nai';
+// li.classList.add('first-1')
+// ul2.appendChild(li)
+
+//inside/before.....
+// const ul2 =document.querySelector('ul');
+// const li4 =document.getElementById('li-item2');
+// const list5 =document.createElement('li5ultra');
+// list5.innerText =`hello i m robot 2.0`;
+// ul2.insertBefore(list5,li4);
+
+
+//eventhandler niye kaj.......
+
+//click.......
+// let sum =0
+// const firstElement =document.getElementById('count');
+// function clickMe(){ 
+//       sum += 1;
+//        firstElement.innerText=sum;
+// }
+
+// function clickMe2(){ 
+//       sum -= 1;
+//        firstElement.innerText= sum;
+// }
+
+// inputext.........
+// let sirialCount =1; 
+// function loadData(){
+//      const firstelemnt =document.getElementById('input-btn');
+//      const inputFile =document.getElementById('input-field');
+//      const inputext =document.getElementById('input-taxt');
+//      const field =inputFile.value;
+//      const tage =document.createElement('p');
+//      tage.innerText =sirialCount +'.'+ inputFile.value;
+//      sirialCount ++;
+//      inputFile.value ='';
+//      inputext.appendChild(tage);   
+// }
+
+// keyboard.........
+
+document.getElementById('input-btn').addEventListener('keyup', function(e){
+
+})
+
